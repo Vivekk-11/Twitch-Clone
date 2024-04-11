08bb20481e26dce31c5c9e589e63806f0d8f2bbb
vivek <chimnanivivek14@gmail.com>
2024-04-11 18:07:03 +0530

    sidebar component

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Navbar } from "./_components/navbar";
+import { Sidebar } from "./_components/sidebar";
+import { Container } from "./_components/containter";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <div>{children}</div>
+      <div className="flex h-full pt-20">
+        <Sidebar />
+        <Container>{children}</Container>
+      </div>
     </>
   );
 };
