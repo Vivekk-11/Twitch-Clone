0910abdcaa5eb96c5ae5ccd484dd65d06d6aa464
vivek <chimnanivivek14@gmail.com>
2024-04-12 18:19:32 +0530

    update chat functionality

---
+++ actions/stream.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { getSelf } from "@/lib/auth-service";
+import { db } from "@/lib/db";
+import { Stream } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+export const updateStream = async (values: Partial<Stream>) => {
+  try {
+    const self = await getSelf();
+    const selfStream = await db.stream.findUnique({
+      where: {
+        userId: self.id,
+      },
+    });
+
+    if (!selfStream) throw new Error("Stream not found!");
+
+    const { isChatEnabled, isChatDelayed, isChatFollowersOnly, name } = values;
+
+    const validData = {
+      name,
+      isChatEnabled,
+      isChatDelayed,
+      isChatFollowersOnly,
+    };
+
+    const stream = await db.stream.update({
+      where: {
+        id: selfStream.id,
+      },
+      data: {
+        ...validData,
+      },
+    });
+
+    revalidatePath("/");
+    revalidatePath(`/${self.username}`);
+    revalidatePath(`/u/${self.username}`);
+    revalidatePath(`/u/${self.username}/chat`);
+    return stream;
+  } catch (error) {
+    throw new Error("Something went wrong, please try again!");
+  }
+};
