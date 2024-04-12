4e58905282f406719a0e9b5edd68cb02c80991c9
vivek <chimnanivivek14@gmail.com>
2024-04-12 09:48:26 +0530

    fixed a minor hydration error

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -2,13 +2,30 @@
 
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
+import { useEffect, useState } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const Wrapper = ({ children }: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
   const { collapsed } = useSidebar((state) => state);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted)
+    return (
+      <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2d2e35] z-50">
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
+
   return (
     <aside
       className={cn(
