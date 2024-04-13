9f241943450d66a126cde47636bf3bf23b50403a
vivek <chimnanivivek14@gmail.com>
2024-04-13 13:16:25 +0530

    fixed a minor error while fetching isLive property

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -112,7 +112,9 @@ export const getFollowedUsers = async () => {
       include: {
         following: {
           include: {
-            stream: true,
+            stream: {
+              select: { isLive: true },
+            },
           },
         },
       },
