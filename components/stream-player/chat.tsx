8960e35dca66c062cf71a2d6dd21db29b36c7453
vivek <chimnanivivek14@gmail.com>
2024-04-13 16:38:26 +0530

    chat toggle functionality

---
+++ components/stream-player/chat.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { ConnectionState } from "livekit-client";
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+import {
+  useChat,
+  useConnectionState,
+  useRemoteParticipant,
+} from "@livekit/components-react";
+import { useMediaQuery } from "usehooks-ts";
+import { useEffect, useMemo, useState } from "react";
+import { ChatHeader } from "./chat-header";
+
+interface Props {
+  hostName: string;
+  hostIdentity: string;
+  viewerName: string;
+  isFollowing: boolean;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+}
+
+export const Chat = ({
+  hostIdentity,
+  hostName,
+  viewerName,
+  isFollowing,
+  isChatDelayed,
+  isChatEnabled,
+  isChatFollowersOnly,
+}: Props) => {
+  const [value, setValue] = useState("");
+  const { chatMessages: messages, send } = useChat();
+  const matches = useMediaQuery("(max-width:1024px)");
+  const { variant, onExpand } = useChatSidebar((state) => state);
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+  const isOnline = participant && connectionState === ConnectionState.Connected;
+  const isHidden = !isChatEnabled || !isOnline;
+
+  const reversedMessages = useMemo(() => {
+    return messages.sort((a, b) => b.timestamp - a.timestamp);
+  }, [messages]);
+
+  const onSubmit = () => {
+    if (!send) return;
+    send(value);
+    setValue("");
+  };
+
+  const onChange = (value: string) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if (matches) {
+      onExpand();
+    }
+  }, [matches, onExpand]);
+
+  return (
+    <div className="flex flex-col bg-background border-l border-b pt-0 h-[calc(100vh-80px)]">
+      <ChatHeader />
+    </div>
+  );
+};
