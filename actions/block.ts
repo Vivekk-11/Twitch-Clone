6dbe13f3f1f9288dcffb59566063a0872e209aed
vivek <chimnanivivek14@gmail.com>
2024-04-15 12:41:24 +0530

    fixed some minor issues and completed the entire project

--- actions/block.ts
+++ actions/block.ts
@@ -34,20 +34,10 @@ export const onBlock = async (id: string) => {
 };
 
 export const onUnblock = async (id: string) => {
-  // TODO: Adapt to disconnect from livestream
-  // TODO: Allow the ability to kick the guest
+  const self = await getSelf();
+  const unblockedUser = await unBlockUser(id);
 
-  try {
-    const unblockedUser = await unBlockUser(id);
+  revalidatePath(`/u/${self.username}/community`);
 
-    revalidatePath("/");
-
-    if (unblockedUser) {
-      revalidatePath(`/${unblockedUser.blocked.username}`);
-    }
-
-    return unblockedUser;
-  } catch (error) {
-    throw new Error("Something went wrong!");
-  }
+  return unblockedUser;
 };
